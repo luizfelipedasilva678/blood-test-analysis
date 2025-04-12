4871e3fe54c5ebff64aeb735317bc325fe67abc6
Luiz Felipe <luizfelipedasilva678@gmail.com>
2025-04-12 20:31:03 -0300

    Add login and register templates

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 @app.route("/")
-def hello():
-    return render_template("base.html")
+def dashboard():
+    if "userid" not in session:
+        return redirect(url_for("register"))
+
+    return render_template("index.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
